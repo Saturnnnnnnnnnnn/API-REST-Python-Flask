cfd635bd765c0478c656ce4d7ce93ae2edc71aed
Saturnnnnnnn <hamza.shermatov0504@gmail.com>
2023-11-15 17:45:06 +0300

    Connexion and SQLAlchemy

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/saturn/flask_api/venv"
+set -gx VIRTUAL_ENV "/home/saturn/API-REST-Python-Flask/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
